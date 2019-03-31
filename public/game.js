cdc148c32e5a25e49d65859a984bd1be7515b498
champfish <cj.duffee@gmail.com>
2019-03-31 10:07:32 -0500

    Logging

--- public/game.js
+++ public/game.js
@@ -604,7 +604,7 @@ function returnInTextMode(){
 function mouseDown(e) {
     var eX = e.touches[0].clientX;
     var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    console.log(eX+' '+w);
+    alert(eX+' '+w);
     if(eX<w/3){
         left = true;
     }
